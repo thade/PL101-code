f981175b50ee348d71ec8cb1ca2d4c1598ef22ad
Thade O'Connor <thade.oconnor@gmail.com>
2012-04-25 14:52:11 +0930

    added quote and comments

--- scheem/test.js
+++ scheem/test.js
@@ -16,6 +16,7 @@ assert.deepEqual(parse("+"), "+",
     "parse +");
 assert.deepEqual(parse("(+ x 3)"), ["+", "x", "3"],
     "parse (+ x 3)");
-assert.deepEqual(parse("(+ 1 (f x 3 y))"), 
+assert.deepEqual(parse("(+ 1   (f x 3 y))"), 
     ["+", "1", ["f", "x", "3", "y"]],
     "parse (+ (1 (f x 3 y))");
+assert.deepEqual( parse("'(a b c)"), ["quote", ["a", "b", "c"]] );
